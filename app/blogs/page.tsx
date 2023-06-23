04427d207ee1e4a4a4883a716c9cab821f0a2897
deepyes02 <deepesh.dhakal@digitalwallet.co.jp>
2023-06-23 18:46:49 +0900

    Post data successful with Frontend

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,23 +1,56 @@
 'use client';
+import { useState } from "react";
 import "./blogs.scss";
 
+async function postBlog (formData: object){
+	const url = "http://localhost/api/blogs";
+
+	const response = await fetch(url, {
+		method: "POST",
+		mode: "cors",
+		cache: "no-cache",
+		credentials: "same-origin",
+		headers: {
+			"Content-Type" : "application/json"
+		},
+		redirect : "follow",
+		referrerPolicy: "no-referrer",
+		body: JSON.stringify(formData)
+	});
+	return response.json();
+}
+
 export default function Page() {
+	const [slug, setSlug] = useState("");
+	const [title, setTitle] = useState("");
+	function onClickHandler(){
+		setTitle(title);
+		setSlug(slug);
+		postBlog({title: title, slug: slug})
+	}
+
+	function onChangeTitleHandler(e: any){
+		setTitle(e.target.value)
+	}
+	function onChangeSlugHandler(e: any){
+		setSlug(e.target.value)
+	}
 	return (
-		<>
+		<div className="formWrapper">
 			<h1 className="__title">Add a new Blog</h1>
 			<div className="blog_adder">
 				<div className="wrapper">
 					<label htmlFor="title">Title</label>
-					<input type="text" name="title" placeholder="Add a title" />
+					<input type="text" name="title" value={title} onChange={onChangeTitleHandler}/>
 				</div>
 				<div className="wrapper">
-					<label htmlFor="blog_content">Content</label>
-					<textarea></textarea>
+					<label htmlFor="blog_content">Slug</label>
+					<input type="text" value={slug} onChange={onChangeSlugHandler}/>
 				</div>
 				<div className="wrapper">
-					<button>Upload</button>
+					<button onClick={onClickHandler}>Upload</button>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
